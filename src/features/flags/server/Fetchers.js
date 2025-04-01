e1a546cf4a6ed2c4e4db90e073430a305a3ef3c6
Priyesh Soni <priyesh9792@gmail.com>
2025-04-02 01:17:20 +0530

    remove extra errors

--- src/features/flags/server/Fetchers.js
+++ src/features/flags/server/Fetchers.js
@@ -3,15 +3,9 @@ const API_ENDPOINT = "https://xcountries-backend.azurewebsites.net/all";
 export const fetchCountryFlag = async () => {
   try {
     const res = await fetch(`${API_ENDPOINT}`);
-    if (!res.ok) {
-      console.log(`HTTP error! Status :${res.status}`);
-    }
 
     const data = await res.json();
 
-    if (!Array.isArray(data)) {
-      console.log("Unexpected response format");
-    }
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
