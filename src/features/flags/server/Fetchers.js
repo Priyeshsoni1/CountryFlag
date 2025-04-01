783efc2e3ec1a236993b2f86adec599a4bf46d9a
Priyesh Soni <priyesh9792@gmail.com>
2025-04-02 01:07:50 +0530

    Remove throw error

--- src/features/flags/server/Fetchers.js
+++ src/features/flags/server/Fetchers.js
@@ -4,17 +4,17 @@ export const fetchCountryFlag = async () => {
   try {
     const res = await fetch(`${API_ENDPOINT}`);
     if (!res.ok) {
-      throw new Error(`HTTP error! Status :${res.status}`);
+      console.log(`HTTP error! Status :${res.status}`);
     }
 
     const data = await res.json();
 
     if (!Array.isArray(data)) {
-      throw new Error("Unexpected response format");
+      console.log("Unexpected response format");
     }
     return data;
   } catch (err) {
     console.log("Error in Fetching", err);
-    throw err;
+    // throw err;
   }
 };
